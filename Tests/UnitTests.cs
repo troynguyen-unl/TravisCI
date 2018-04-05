30abf8cd5a7b872c5c745c7452b6abb54f10464b
troynguyen8@gmail.com <troynguyen8@gmail.com>
2018-04-05 10:52:57 -0500

    removed redundant Program reference by including Program.cs statically

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NUnit.Framework;
+using static TravisCILab.Program;
 
 namespace TravisCILab
 {
@@ -9,81 +10,81 @@ namespace TravisCILab
         [Test]
         public void Add_Valid()
         {
-            Assert.AreEqual(3, Program.Add("1", "2"));
-            Assert.AreEqual(5, Program.Add("3", "2"));
-            Assert.AreEqual(12, Program.Add("5", "7"));
+            Assert.AreEqual(3, Add("1", "2"));
+            Assert.AreEqual(5, Add("3", "2"));
+            Assert.AreEqual(12, Add("5", "7"));
         }
 
         [Test]
         public void Add_Invalid()
         {
-            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
-            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
-            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+            Assert.Throws<FormatException>(() => Add("1", "a"));
+            Assert.Throws<FormatException>(() => Add("a", "1"));
+            Assert.Throws<FormatException>(() => Add("a", "a"));
         }
 
         [Test]
         public void Add_Null()
         {
-            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
-            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
-            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+            Assert.Throws<ArgumentNullException>(() => Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Add(null, null));
         }
 
         [Test]
         public void Subtract_Valid()
         {
-            Assert.AreEqual(-1, Program.Subtract("1", "2"));
-            Assert.AreEqual(1, Program.Subtract("3", "2"));
-            Assert.AreEqual(-2, Program.Subtract("5", "7"));
+            Assert.AreEqual(-1, Subtract("1", "2"));
+            Assert.AreEqual(1, Subtract("3", "2"));
+            Assert.AreEqual(-2, Subtract("5", "7"));
         }
 
         [Test]
         public void Subtract_Invalid()
         {
-            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
-            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
-            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+            Assert.Throws<FormatException>(() => Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Subtract("a", "a"));
         }
 
         [Test]
         public void Subtract_Null()
         {
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
-            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+            Assert.Throws<ArgumentNullException>(() => Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Subtract(null, null));
         }
 
         [Test]
         public void Multiply_Valid()
         {
-            Assert.AreEqual(2, Program.Multiply("1", "2"));
-            Assert.AreEqual(6, Program.Multiply("3", "2"));
-            Assert.AreEqual(35, Program.Multiply("5", "7"));
+            Assert.AreEqual(2, Multiply("1", "2"));
+            Assert.AreEqual(6, Multiply("3", "2"));
+            Assert.AreEqual(35, Multiply("5", "7"));
         }
 
         [Test]
         public void Multiply_Invalid()
         {
-            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
-            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
-            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+            Assert.Throws<FormatException>(() => Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Multiply("a", "a"));
         }
 
         [Test]
         public void Multiply_Null()
         {
-            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
-            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
-            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+            Assert.Throws<ArgumentNullException>(() => Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Multiply(null, null));
         }
 
         [Test]
         public void Divide_Valid()
         {
-            Assert.AreEqual(0.5, Program.Divide("1", "2"));
-            Assert.AreEqual(1.5, Program.Divide("3", "2"));
-            Assert.AreEqual((double) 5 / 7, Program.Divide("5", "7"));
+            Assert.AreEqual(0.5, Divide("1", "2"));
+            Assert.AreEqual(1.5, Divide("3", "2"));
+            Assert.AreEqual((double) 5 / 7, Divide("5", "7"));
         }
 
         [Test]
